a7dd9daad04b8257423b4c08bebc8ee603447c8a
AvinashTangudu17 <avinash.tang1711@gmail.com>
2025-02-01 15:58:01 +0530

    Commit to Day1

---
+++ wipro_java_project/src/com/wipro/java/Flower.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class Flower {
+
+	String name;
+	String color;
+	int price;
+	
+	Flower(String name, String color, int price) {
+		this.name=name;
+		this.color=color;
+		this.price=price;
+	}
+	
+	void display() {
+		System.out.println("Flower name:"+name+" Color:"+color+" Price:"+price);
+	}
+	public static void main(String[] args) {
+		Flower flowerobj1 = new Flower("Tulip","white",20);
+		Flower flowerobj2 = new Flower("Rose","pink",30);
+		flowerobj1.display();
+		flowerobj2.display();
+
+	}
+
+}
