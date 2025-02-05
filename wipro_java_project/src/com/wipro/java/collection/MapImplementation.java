101b12d5d203828ffbe9687d1b5526e5a00ee9e4
AvinashTangudu17 <avinash.tang1711@gmail.com>
2025-02-05 16:15:14 +0530

    Day 4

--- wipro_java_project/src/com/wipro/java/collection/MapImplementation.java
+++ wipro_java_project/src/com/wipro/java/collection/MapImplementation.java
@@ -1,15 +1,9 @@
-/**
- * 
- */
 package com.wipro.java.collection;
 
-/**
- * 
- */
 import java.util.HashMap;
 import java.util.Map;
 
-public class MapCode {
+public class MapImplementation {
 	public static void main(String[] args) {
        Map<Integer, String> map = new HashMap<>();
 	
