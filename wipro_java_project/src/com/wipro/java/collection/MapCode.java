e0f22b8b87b3531cbcc64c895c8f473fef8c2e12
AvinashTangudu17 <avinash.tang1711@gmail.com>
2025-02-05 15:44:54 +0530

    Day 4

---
+++ wipro_java_project/src/com/wipro/java/collection/MapCode.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.wipro.java.collection;
+
+/**
+ * 
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapCode {
+	public static void main(String[] args) {
+       Map<Integer, String> map = new HashMap<>();
+	
+	   map.put(1, "Parrot");
+	   map.put(2, "Eagle");
+	   map.put(3, "Sparrow");
+
+	//Accessing values by key
+	System.out.println("Key 1: " + map.get(1));
+	System.out.println("Key 2: " + map.get(2));
+	
+	//Checking if a key exists in the map
+	if (map.containsKey(3)) {
+		System.out.println("Key 3 exists with value: " + map.get(3));
+		
+	}
+	
+	System.out.println("\nIterating through the map:");
+	for(Map.Entry<Integer, String> entry : map.entrySet()) {
+		System.out.println(entry.getKey() + ":" + entry.getValue());
+	}
+	map.remove(2);
+	
+	System.out.println("\nMap after removal of key 2: ");
+	for (Map.Entry<Integer, String> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ": " + entry.getValue());
+	}
+	}
+}
